1fb3b3f08fde1e434abe341c5659e91387538775
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-12-06 12:55:51 +0100

    fix code style

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -24,7 +24,7 @@ def canUnlockAll(boxes):
     Returns:
       True if all boxes can be opened, else return False
     """
-    if len(boxes) <= 1 or boxes == [[]]:
+    if len(boxes) == 0 or boxes == [[]]:
         return True
     visited = set()
     dfs(boxes, 0, visited)
