f324aa47c2cd4e189e04ef67db7d7740e3b2dae6
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-12-06 12:54:59 +0100

    fix code style

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,18 +1,33 @@
 #!/usr/bin/python3
+""" Lockboxes """
+
 
 def dfs(boxes, node, visited):
-  if node in visited:
-    return
-  visited.add(node)
-  for box in boxes[node]:
-    if box not in visited:
-      dfs(boxes, box, visited)
+    """depth first search
+    Args:
+      boxes: list of all boxies
+      node: current box
+      visited: set contains all visited nodes
+    """
+    if node in visited:
+        return
+    visited.add(node)
+    for box in boxes[node]:
+        if box not in visited:
+            dfs(boxes, box, visited)
+
 
 def canUnlockAll(boxes):
-  if len(boxes) <= 1 or boxes == [[]]:
-    return True
-  visited = set()
-  dfs(boxes, 0, visited)
-  if (len(visited) == len(boxes)):
-    return True
-  return False
+    """lockboxes solution
+    Args:
+      boxes: list of boxes
+    Returns:
+      True if all boxes can be opened, else return False
+    """
+    if len(boxes) <= 1 or boxes == [[]]:
+        return True
+    visited = set()
+    dfs(boxes, 0, visited)
+    if (len(visited) == len(boxes)):
+        return True
+    return False
