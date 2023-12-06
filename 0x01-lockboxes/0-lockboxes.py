44dc2d5ada9e8076f78605dbc0f29883192d044c
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-12-06 12:42:53 +0100

    lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def dfs(boxes, node, visited):
+  if node in visited:
+    return
+  visited.add(node)
+  for box in boxes[node]:
+    if box not in visited:
+      dfs(boxes, box, visited)
+def canUnlockAll(boxes):
+  visited = set()
+  dfs(boxes, 0, visited)
+  if (len(visited) == len(boxes)):
+    return True
+  return False
