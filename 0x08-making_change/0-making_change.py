97c443aeb152c69a8642e3b88928eecc0b85a5d1
hamzaboutaleb <hamza-boutaleb@live.fr>
2024-02-11 20:34:12 +0100

    make change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" making change module
+"""
+
+
+def makeChange(coins, total):
+    """number of coins needed to  meet the total
+    """
+    if total <= 0:
+        return 0
+
+    if len(coins) is 0:
+        return -1
+
+    coins = sorted(coins)
+
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for coin in coins:
+        if coin > total:
+            break
+        for i in range(coin, total + 1):
+                dp[i] = min(dp[i], dp[i - coin] + 1)
+
+    return dp[total] if dp[total] != float('inf') else -1
